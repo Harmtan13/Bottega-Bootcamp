27e585e19290ad52b94194cdece9062531bc88b4
Tanner Harman <harmtan2@gmail.com>
2017-12-22 01:22:24 +0000

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,15 +1,22 @@
 class PortfoliosController < ApplicationController
-  layout "portfolio"
-    def index
-      @portfolio_items = Portfolio.all
-    end
-    
-    def new
-      @portfolio_item = Portfolio.new
-      3.times { @portfolio_item.technologies.build }
-    end
-    
-    def create
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
+  layout 'portfolio'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+
+  def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
@@ -20,45 +27,44 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-    
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'The Record successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
-  
+
   def show
-     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
-    
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
+    # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Record was remobed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
-  
+
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
-                                      :subtitle, 
-                                      :body, 
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
-  
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
