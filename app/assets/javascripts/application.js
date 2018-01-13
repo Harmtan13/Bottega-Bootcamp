cbdda532972f1a3d47e39c5eb14e0eb8ee3909cc
Tanner Harman <harmtan2@gmail.com>
2018-01-13 23:05:00 +0000

    Updated images for about me page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require rails-ujs
 //= require jquery3
-//= require bootstrap-sprockets
-//= require jquery_ujs
+//= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
-//= require popper
+//= require cable
+//= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
