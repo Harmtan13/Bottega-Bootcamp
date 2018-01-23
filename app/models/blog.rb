98184d33d932f4720d41abb2a699475633f9c298
Tanner Harman <harmtan2@gmail.com>
2018-01-23 19:22:05 +0000

    added validations for topics in blogs and added a topic form element

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,14 +2,8 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
-  after_initialize :set_defaults
-  
-  def set_defaults
-     self.topic_id = 1
-  end
 
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :topic
 
   belongs_to :topic
   
