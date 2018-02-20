53d83a05b5b94b68f163f567a55cda64a2ebc66a
Harmtan13 <Harmtan2@gmail.com>
2018-02-19 20:48:25 -0700

    Implemented custom scope for Posts by users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,54 +1,53 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @posts = current_user.posts
+  def index
+    @posts = Post.posts_by(current_user)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+        redirect_to @post, notice: 'Your post was created successfully'
+    else
+        render :new
     end
+  end
+
+  def edit
+    authorize @post
+  end
+
+  def update
+    authorize @post
+
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post was edited successfully'
+    else
+      render :edit
+    end
+  end
     
-    def new
-        @post = Post.new
-    end
-    
-    def create
-        @post = Post.new(post_params)
-        @post.user_id = current_user.id
-        
-        if @post.save
-            redirect_to @post, notice: 'Your post was created successfully'
-        else
-            render :new
-        end
-    end
+  def show
+  end
 
-    def edit
-      authorize @post
-    end
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
+  end
 
-    def update
-      authorize @post
+  private 
+  def post_params
+      params.require(:post).permit(:date, :rationale, :status)
+  end
 
-      if @post.update(post_params)
-        redirect_to @post, notice: 'Your post was edited successfully'
-      else
-        render :edit
-      end
-    end
-        
-    def show
-    end
-
-    def destroy
-      @post.delete
-      redirect_to posts_path, notice: 'Your post was deleted successfully'
-    end
-    
-    private 
-        def post_params
-            params.require(:post).permit(:date, :rationale, :status)
-        end
-        
-        def set_post
-            @post = Post.find(params[:id])
-        end
-
+   def set_post
+      @post = Post.find(params[:id])
+  end
 end
