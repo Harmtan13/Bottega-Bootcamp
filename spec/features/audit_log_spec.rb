2a47877704e7cf594bb58c3c2d7b16d480651911
Harmtan13 <Harmtan2@gmail.com>
2018-02-26 22:38:41 -0700

    Blocked non-admin users from accessing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -16,11 +16,17 @@ describe 'AuditLog Feature' do
 
     it 'render audit log content' do
       visit audit_logs_path
-      expect(page).to have_content(/HARMAN/)
+    expect(page).to have_content(/HARMAN/)
     end
 
-    xit 'cannot be accessed by non admin users' do
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
       
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
