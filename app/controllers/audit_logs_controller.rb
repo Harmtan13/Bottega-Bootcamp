c2b35813fef98004c8f5ac769d7aa7372907a731
Harmtan13 <Harmtan2@gmail.com>
2018-02-27 10:28:36 -0700

    Implemented AJAX based pagination for posts and audit logs

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,6 +1,6 @@
 class AuditLogsController < ApplicationController
   def index
-    @audit_logs = AuditLog.page(params[:page]).per(15)
+    @audit_logs = AuditLog.page(params[:page]).per(13)
     authorize @audit_logs
   end
 end
