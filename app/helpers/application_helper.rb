dc95c1d39c7f84ea661c5077032d0eaa1286f4e4
Tanner <harmtan2@gmail.com>
2018-03-04 12:32:30 -0700

    UPdated nav to remove components not needed for admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,14 @@ module ApplicationHelper
   def active(path)
     "active" if current_page?(path)
   end
+
+  def employee?
+    current_user.type == 'Employee'
+  end
+
+  def admin?
+    admin_types.include?(current_user.type)
+  end
 end
 
 module PostsHelper
