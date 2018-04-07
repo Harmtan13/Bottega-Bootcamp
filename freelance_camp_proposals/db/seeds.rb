2b1a41e29f2c673ff3004cb48424dd4b8a7e614b
Harmtan13 <Harmtan2@gmail.com>
2018-04-07 17:48:05 -0600

    Connected the Proposals Front-End with Proposals Backend Rails App

--- freelance_camp_proposals/db/seeds.rb
+++ freelance_camp_proposals/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |proposal|
+  Proposal.create!(
+    customer: "Customer #{proposal+1}",
+    portfolio_url: 'http://portfolio.jordanhudgens.com',
+    tools: 'Ruby on Rails, Angular 2, and Postgres',
+    estimated_hours: (rand(80) + proposal),
+    hourly_rate: rand(50)+100,
+    weeks_to_complete: rand(20),
+    client_email: 'tharman@familyguardfiltering.com',
+  )
+end
+
+puts "Created 10 Proposals"
